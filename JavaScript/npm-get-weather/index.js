a27480ec56a412f0b2354c2ee04fd181b85b47c3
chunge <1362598656@qq.com>
2019-11-07 17:02:17 +0800

    fix: :bug:　修复接口请求错误的问题

--- JavaScript/npm-get-weather/index.js
+++ JavaScript/npm-get-weather/index.js
@@ -1,30 +1,39 @@
 #!/usr/bin/env node
-let axios = require('axios')
-console.log('Hi, this is weather report app')
+let axios = require('axios');
+console.log('Hi, this is weather report app');
+const URl = 'http://api.jirengu.com/getWeather.php';
 
 let getWeather = function () {
-  let params = {
-    key: 'study_javascript_in_jirengu.com'
-  }
-  if (process.argv[2] && typeof process.argv[2] === 'string') {
-    params.location = process.argv[2]
-  }
+    let params = {};
+    if (process.argv[2] && typeof process.argv[2] === 'string') {
+        params.city = process.argv[2];
+    }
 
-  return new Promise(function (resolve, reject) {
-    axios.get('https://weixin.jirengu.com/weather', { params }).then(function (resp) {
-      resolve(resp.data)
-    }).catch(function (err) {
-      reject(err)
-    })
-  })
-}
+    return new Promise(function (resolve, reject) {
+        axios.get(URl, {params}).then(function (resp) {
+            resolve(resp.data);
+        }).catch(function (err) {
+            reject(err);
+        });
+    });
+};
 
 getWeather().then(function (data) {
-  console.log("当前城市： ", data.weather[0].city_name)
-  console.log("当前天气： ", data.weather[0].now.text)
-  console.log("当前温度： ", `${data.weather[0].now.temperature}℃`)
-  console.log("当前湿度： ", `${data.weather[0].now.humidity}%`)
-  console.log("出行建议： ", data.weather[0].today.suggestion.travel.details)
-})
+    let {
+        currentCity,
+        weather_data,
+        pm25,
+        index
+    } = data.results[0];
+    let {
+        weather,
+        temperature
+    } = weather_data[0];
+    console.log('当前城市： ', currentCity);
+    console.log('当前天气： ', weather);
+    console.log('当前温度： ', temperature);
+    console.log('当前PM25： ', pm25);
+    console.log('出行建议： ', index[3].des);
+});
 
-module.exports = getWeather
+module.exports = getWeather;
